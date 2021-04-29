0ebb6d435c9afd4dbf99fe678534116100f7c33a
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-04-29 16:28:01 +0200

    Tailwind CSS - Tailwind Config

--- Tailwind_CSS_Tutorial/README.md
+++ Tailwind_CSS_Tutorial/README.md
@@ -7,3 +7,7 @@ Framework packed with utility classes
 1. `npm init -y`, `npm install tailwindcss`, `npm install autoprefixer`
 2. define script `"build-css": "tailwindcss build src/styles.css -o public/styles.css"`
 3. Add tailwind directives to `src/styles.css` — here you can extend tailwind
+
+## Extending Tailwind
+
+run `npx tailwindcss init --full` and change fonts etc
---
+++ Tailwind_CSS_Tutorial/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: "#F3F3F8",
+                secondary: {
+                    100: "#E2D2D5",
+                    200: "#888883",
+                },
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
